6573cb09f174091436774252d069c4e294508ce7
Azique <a3uk.cccp@gmail.com>
2013-05-30 15:38:14 +0400

    Modified ov534 driver. 100 fps at QVGA only

---
+++ driver.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+cd /usr/src
+apt-get update
+apt-get install -y kernel-package build-essential linux-source
+tar --bzip2 -xvf linux-source-*.tar.bz2
+ln -s `find . -type d -name "linux-source-*"` linux
+
+cd /usr/src/linux/drivers/media/video/gspca
+mv ov534.c ov534.c.old
+cp ~/Desktop/ov534.c /usr/src/linux/drivers/media/video/gspca
+
+
+cd /usr/src
+cp -p linux-headers-$(uname -r)/Module.symvers linux
+
+cd /usr/src/linux
+make oldconfig
+make modules_prepare
+make SUBDIRS=drivers/media/video/gspca modules 
+
+
+cd /usr/src/linux
+cp -p drivers/media/video/gspca/gspca_main.ko /lib/modules/$(uname -r)/kernel/drivers/media/video/gspca
+cp -p drivers/media/video/gspca/gspca_ov534.ko /lib/modules/$(uname -r)/kernel/drivers/media/video/gspca
+depmod
+
+
+modprobe -r gspca_ov534 gspca_main
+modprobe gspca_ov534
